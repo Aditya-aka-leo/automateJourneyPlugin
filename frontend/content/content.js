8930f8b5c85c768674c6d1e7147103c0a0ff274b
Aditya <72493902+Aditya-aka-leo@users.noreply.github.com>
2026-06-03 21:15:55 +0530

    blur event for react framework

--- frontend/content/content.js
+++ frontend/content/content.js
@@ -3091,7 +3091,8 @@ async function performStep(step) {
     
     el.dispatchEvent(new Event("input", { bubbles: true }));
     el.dispatchEvent(new Event("change", { bubbles: true }));
-    
+    el.dispatchEvent(new FocusEvent("blur", { bubbles: true }));
+
     // Wait for DOM to stabilize, but don't fail if it doesn't
     const postDom = await waitForDOMStable();
     if (!postDom.ok) {
